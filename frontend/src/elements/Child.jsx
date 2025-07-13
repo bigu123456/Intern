28cbab69e92166e9652974ea79d98402de607d6f
bigu123456 <bigyanacharya905@gmail.com>
2025-07-13 09:27:23 +0545

    learning props

--- frontend/src/elements/Child.jsx
+++ frontend/src/elements/Child.jsx
@@ -1,55 +1,92 @@
 import React from 'react'
-import {Table,Button} from "antd"
+import { Table} from "antd"
+import {Button, Form,Input} from "antd";
+
+
 
 const Child = (props) => {
-const { persons,message } = props;
-
-  console.log(persons);
+  const{user,addperson}=props
+  console.log(user);
 
   const columns=[
-
     {
-    title:"id",
-    dataIndex:"id",
-    key:"id"
-
+      title:"id",
+      dataIndex:"id",
+      key:"id"
     },
      {
-    title:"name",
-    dataIndex:"name",
-    key:"name"
-
+      title:"name",
+      dataIndex:"name",
+      key:"name"
     },
      {
-    title:"age",
-    dataIndex:"age",
-    key:"age"
-
+      title:"age",
+      dataIndex:"age",
+      key:"age"
     },
-     {
-    title:"city",
-    dataIndex:"city",
-    key:"city"
+    {
+      title:"city",
+      dataIndex:"city",
+      key:"city"
 
     },
    
+
   ]
 
   
-
-    const sendData = () => {
-    if (message) {
-     
-     message("hello")
-    }
-  };
-
   return (
     <>
-      <Table dataSource={persons} columns={columns} rowKey="id" />
-      <Button type="primary" onClick={sendData}> Send Message to Parent</Button>
-    </>
-  );
-};
+    
+    <div className='bg-yellow-300 rounded-sm shadow-sm py-5 w-1/5'>
+      <Form  onFinish={addperson}>
+<Form.Item 
+name="name"
+rules={[{required:true, message:"enetr your name"}]}
+>
+  <Input
+  placeholder='enetr your name'
+  className='w-40'
+  />
 
-export default Child;
+</Form.Item>
+<Form.Item
+name="age" 
+
+rules={[{required:true, message:"enetr your age"}]}
+>
+  <Input
+  placeholder='enetr your name'
+  className='w-40'
+  />
+
+</Form.Item>
+<Form.Item 
+name="city"
+
+rules={[{required:true, message:"enetr your city"}]}
+>
+  <Input
+  placeholder='enetr your name'
+  className='w-40'
+  />
+
+</Form.Item>
+<Button type='primary' htmlType='submit' >add</Button>
+
+    </Form>
+     
+    </div>
+<Table dataSource={user} columns={columns} rowKey="id"></Table>
+
+    
+  
+   
+    </>
+  )
+}
+
+
+
+
+export default Child
--- frontend/src/elements/Parent.jsx
+++ frontend/src/elements/Parent.jsx
@@ -1,34 +1,51 @@
-import React, { useState } from 'react';
+import React from 'react'
+import { useState } from 'react'
 import Child from './Child';
 
-const Parent = () => {
-  const [message, setMessage] = useState('');
 
- const users = [
-  { id: 1, name: "Alice", age: 25, city: "Kathmandu" },
-  { id: 2, name: "Bob", age: 30, city: "Pokhara" },
-  { id: 3, name: "Charlie", age: 28, city: "Lalitpur" },
-  { id: 4, name: "David", age: 22, city: "Bhaktapur" },
-  { id: 5, name: "Eve", age: 35, city: "Butwal" },
-  { id: 6, name: "Frank", age: 26, city: "Biratnagar" },
-  { id: 7, name: "Grace", age: 27, city: "Dharan" },
-  { id: 8, name: "Heidi", age: 29, city: "Hetauda" },
-  { id: 9, name: "Ivan", age: 31, city: "Janakpur" },
-  { id: 10, name: "Judy", age: 24, city: "Nepalgunj" },
-  {id:11,name:"Bigyan", age:23,city:"morang"},
+const Parent = () => {
+  const [person, setperson] = useState("");
+  const array=[
+    {id:1, name:"Bigyan",age:22,city:"kathmandu"},
+    {id:2, name:"Biswash",age:22,city:"kathmandu"},
+    {id:3, name:"ujwal",age:22,city:"kathmandu"},
+    {id:4, name:"saroj",age:22,city:"kathmandu"},
+    {id:5, name:"agaman",age:22,city:"kathmandu"},
+    {id:6, name:"sujal",age:22,city:"kathmandu"},
+    {id:7, name:"roman",age:22,city:"kathmandu"},
+  ]
+  const [data, setdata] = useState(array)
+  
+  const handlesearch=(e)=>{
+    const values=e.target.value
+    setperson(values);
+  }
+  const search=data.filter(items=>items.name.toLowerCase().includes(person.toLowerCase()))
+  console.log("here is  array",search)
+
+  const handleaddperson = (values) => {
+  const newPerson = {
+    id: data.length + 1,
+    ...values
     
-];
-const handleMessage = (data) => {
-    setMessage(data);
   };
+  setdata((prev) => [...prev, newPerson]);
+};
 
   return (
     <>
+    <h1>learning props and filter and add </h1>
+    <input
+    type="text"
+    value={person}
+    onChange={handlesearch}
+    />
+  
     
-      <Child persons={users} message={handleMessage} />
-        <h2>Message from Child: {message}</h2>
+    
+    <Child  user={search} addperson={handleaddperson} />
     </>
-  );
-};
+  )
+}
 
-export default Parent;
+export default Parent
