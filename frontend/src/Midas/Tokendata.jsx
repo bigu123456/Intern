09119431be987435a5c95ac4938638471f6c5047
bigu123456 <bigyanacharya905@gmail.com>
2025-08-17 10:38:07 +0545

    fetching data from api

--- frontend/src/Midas/Tokendata.jsx
+++ frontend/src/Midas/Tokendata.jsx
@@ -18,13 +18,26 @@ const Tokendata = () => {
   };
 
   const columns = [
-    { title: 'ID', dataIndex: 'id', key: 'id' },
+    { title: 'ID', dataIndex: 'id', key: 'id'  },
     { title: 'Name', dataIndex: 'name', key: 'name' },
   ];
 
   return (
     <>
-      <div className='px-10 py-10'>
+<div className="flex flex-col justify-center items-center px-5 py-5 bg-gray-50 shadow-md">
+
+      <p>Government od Nepal</p>
+      <p>ministry of Health and Population</p>
+      <p>Department of health services</p>
+      <p>Health managaemnt department system</p>
+      <p>Public  hospital monthly reporting form</p>
+
+
+    </div>
+   
+
+  
+      <div className='  px-10 py-10 '>
         <Input
           className='py-3 w-32'
           placeholder="Search by name"
@@ -32,26 +45,24 @@ const Tokendata = () => {
           onChange={handleSearch}
         />
       </div>
+       <div className='flex space-x-11 gap-10 px-5 py-5'>
+      <Table
+        className="custom-table"
+        columns={columns} 
+        dataSource={filteredData}
+        
+        rowKey="id"
+      />
+     
       <Table
         className="custom-table"
         columns={columns}
         dataSource={filteredData}
         rowKey="id"
+       
+           
       />
-      <div className='px-10 py-10'>
-        <Input
-          className='py-3 w-32'
-          placeholder="Search by name"
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-      </div>
-      <Table
-        className="custom-table"
-        columns={columns}
-        dataSource={filteredData}
-        rowKey="id"
-      />
+        </div>
     </>
   );
 };
--- frontend/src/elements/Test.jsx
+++ frontend/src/elements/Test.jsx
@@ -1,29 +1,111 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
+import React from "react";
+import { Table } from "antd";
 
 const Test = () => {
-  const [now, setnow] = useState(new Date());
  
-  useEffect(()=>{
-    const timer=setInterval(() => {
-      setnow(new Date())
-      
-      
-    }, 1000);
-    return ()=> clearInterval(timer)
-    
-    
-  },[])
+  const genderColumns = (title) => [
+    {
+      title: "M",
+      dataIndex: `${title}_M`,
+      key: `${title}_M`,
+      align: "center",
+    },
+    {
+      title: "F",
+      dataIndex: `${title}_F`,
+      key: `${title}_F`,
+      align: "center",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "S.N.",
+      dataIndex: "sn",
+      key: "sn",
+      align: "center",
+      fixed: "left",
+    },
+    {
+      title: "Visit Date",
+      dataIndex: "visitDate",
+      key: "visitDate",
+      align: "center",
+    },
+    {
+      title: "<28 Days",
+      children: genderColumns("under28Days"),
+    },
+    {
+      title: "28D-1Yr",
+      children: genderColumns("28D_1Yr"),
+    },
+    {
+      title: "2-4Yrs",
+      children: genderColumns("2_4Yrs"),
+    },
+    {
+      title: "5-14Yrs",
+      children: genderColumns("5_14Yrs"),
+    },
+    {
+      title: "15-19Yrs",
+      children: genderColumns("15_19Yrs"),
+    },
+    {
+      title: "20-29Yrs",
+      children: genderColumns("20_29Yrs"),
+    },
+    {
+      title: "30-39Yrs",
+      children: genderColumns("30_39Yrs"),
+    },
+    {
+      title: "40-49Yrs",
+      children: genderColumns("40_49Yrs"),
+    },
+    {
+      title: "50-59Yrs",
+      children: genderColumns("50_59Yrs"),
+    },
+    {
+      title: "60+Yrs",
+      children: genderColumns("60plus"),
+    },
+    {
+      title: "Total",
+      children: genderColumns("total"),
+    },
+    {
+      title: "Grand Total",
+      children: genderColumns("grandTotal"),
+    },
+  ];
+
+ 
+  const data = [];
+
   return (
-  <>
+    <>
+    <div className="px-10 py-10">
+
+   
+   
   
-<h1 className='flex justify-center items-center bg-yellow-500 h-screen'>{now.toLocaleString()}</h1>
 
+   
+    <Table
+      columns={columns}
+      dataSource={data}
+      bordered
+      scroll={{ x: true }}
+      pagination={true}
+      locale={{ emptyText: "No data! Looks like there is no data." }}
+    />
+     </div>
+    
+      </>
+  );
+};
 
-
-  
-  </>
-  )
-}
-
-export default Test
+export default Test;
